fda2c295dff3829a9485929f155c275735744a01
AlexeyZh <waltafunk@gmail.com>
2024-08-05 21:22:34 +0300

    feat: add person data

---
+++ data/data.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+
+from faker import Faker
+
+fake = Faker()
+
+
+@dataclass(slots=True)
+class PersonData:
+    first_name: str = field(default_factory=lambda: fake.first_name())
+    last_name: str = field(default_factory=lambda: fake.last_name())
+    email: str = field(default_factory=lambda: fake.email())
+    mobile: str = field(default_factory=lambda: fake.phone_number())
+    current_address: str = field(default_factory=lambda: fake.address())
