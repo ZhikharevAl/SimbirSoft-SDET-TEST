5a3d31f5841ade9583e503866ab3d5c463f398df
AlexeyZh <waltafunk@gmail.com>
2024-08-07 22:42:21 +0300

    feat: add ublock

--- conftest.py
+++ conftest.py
@@ -12,10 +12,8 @@ def browser():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    chrome_options.add_argument('--disable-ads')
-    chrome_options.add_argument('--disable-popup-blocking')
-    chrome_options.add_argument('--disable-notifications')
-    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_extension("/tmp/ublock_origin/uBlock0.chromium")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
