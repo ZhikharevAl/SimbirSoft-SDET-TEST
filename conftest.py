7e5afe7ad116ffbae746297ffcc83dd88aee0e8f
AlexeyZh <waltafunk@gmail.com>
2024-08-07 16:57:27 +0300

    feat: add conftest

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import allure
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_teardown(request, browser):
+    allure.dynamic.parameter("browser", browser.capabilities["browserName"])
+    yield
+    item = request.node
+    if item.rep_call.failed:
+        allure.attach(browser.get_screenshot_as_png(),
+                      name=f"screenshot_{item.name}",
+                      attachment_type=allure.attachment_type.PNG)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
