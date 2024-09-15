f1b6d2c49fdd259a81d5f1bc9c1aa78318f0e92a
AlexeyZh <waltafunk@gmail.com>
2024-09-15 23:29:54 +0300

    chore: removed redundant logging from tests

--- tests/test_filling_out_the_form.py
+++ tests/test_filling_out_the_form.py
@@ -4,11 +4,9 @@ import pytest
 import allure
 import os
 from allure_commons.types import Severity
-from logger_config import configure_logger
 from pages.registration_page import RegistrationPage
 from data.data import PersonData
 
-logger = configure_logger(__name__, "test.log")
 
 
 @allure.epic("Тестирование формы регистрации")
@@ -39,11 +37,10 @@ class TestRegistrationForm:
     4. Проверяет успешность отправки
     5. Верифицирует корректность отображения всех введенных данных в модальном окне
     """)
-    def test_fill_registration_form(self, page):
+    def test_fill_registration_form(self, page, caplog, logger):
         logger.info("Начало теста заполнения формы регистрации")
 
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
 
         person = PersonData()
@@ -53,11 +50,9 @@ class TestRegistrationForm:
             self._fill_form_fields(page, person)
 
         with allure.step("Отправка формы"):
-            logger.info("Отправка формы")
             page.submit_form()
 
         with (allure.step("Проверка успешной отправки формы")):
-            logger.info("Проверка успешной отправки формы")
             assert page.EXPECTED_SUCCESS_MESSAGE in page.label_text(), \
                 f"Заголовок не совпадает {page.label_text()}"
             assert page.is_submission_successful(), "Форма не была успешно отправлена"
@@ -65,57 +60,36 @@ class TestRegistrationForm:
         with allure.step("Проверка корректности отображения данных в модальном окне"):
             self._verify_modal_content(page, person)
 
-        logger.info("Все проверки пройдены успешно")
         logger.info("Тест заполнения формы регистрации завершен")
 
     @allure.step("Заполнение полей формы")
     def _fill_form_fields(self, page, person):
-        logger.info(f"Заполнение поля 'Имя' значением {person.first_name}")
+
         page.fill_first_name(person.first_name)
-
-        logger.info(f"Заполнение поля 'Фамилия' значением {person.last_name}")
         page.fill_last_name(person.last_name)
-
-        logger.info(f"Заполнение поля 'Email' значением {person.email}")
         page.fill_email(person.email)
-
-        gender = "Male"
-        logger.info(f"Выбор пола: {gender}")
-        page.select_gender(gender)
-
-        logger.info(f"Заполнение поля 'Мобильный телефон' значением {person.mobile}")
+        page.select_gender('Male')
         page.fill_mobile(person.mobile)
-
-        hobbies = ["Sports", "Reading"]
-        logger.info(f"Выбор хобби: {hobbies}")
-        page.select_hobbies(hobbies)
+        page.select_hobbies(["Sports", "Reading"])
 
         project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = os.path.join(project_root, "images", "Battlestar_Galactica.jpg")
-        logger.info(f"Загрузка изображения {file_path}")
         page.upload_picture(file_path)
 
         date_of_birth = "08-March-1987"
-        logger.info(f"Заполнение поля 'Дата рождения' значением {date_of_birth}")
         page.fill_date_of_birth(date_of_birth)
 
         subjects = ["Maths", "Computer Science"]
-        logger.info(f"Выбор предметов: {subjects}")
         page.fill_subjects(subjects)
-
-        logger.info(f"Заполнение поля 'Текущий адрес' "
-                    f"значением {person.current_address}")
         page.fill_current_address(person.current_address)
 
         state, city = "NCR", "Delhi"
-        logger.info(f"Выбор штата: {state} и города: {city}")
         page.select_state(state)
         page.select_city(city)
 
     @allure.step("Проверка содержимого модального окна")
     def _verify_modal_content(self, page, person):
         actual_modal_content = page.get_submitted_data()
-        logger.info(f"Фактическое содержимое модального окна: {actual_modal_content}")
 
         expected_content = [
             (f"Student Name {person.first_name} {person.last_name}", "Имя студента"),
@@ -133,6 +107,3 @@ class TestRegistrationForm:
         for content, description in expected_content:
             assert content in actual_modal_content, \
                 f"{description} не найден в модальном окне"
-            logger.info(f"Проверка пройдена: {description}")
-
-        logger.info("Все проверки содержимого модального окна пройдены успешно")
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -4,10 +4,8 @@ import allure
 from allure_commons.types import Severity
 
 from data.data import PersonData
-from logger_config import configure_logger
 from pages.registration_page import RegistrationPage
 
-logger = configure_logger(__name__, "test.log")
 
 
 @allure.epic("Тестирование формы регистрации")
@@ -24,6 +22,7 @@ logger = configure_logger(__name__, "test.log")
 """)
 class TestRegistrationForm:
 
+
     @pytest.fixture(scope="function")
     def page(self, browser):
         return RegistrationPage(browser)
@@ -33,19 +32,21 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Имя'")
     @allure.description("Этот тест проверяет корректность "
                         "заполнения поля 'Имя' в форме регистрации.")
-    def test_first_name(self, page):
+    def test_first_name(self, page, caplog, logger):
         logger.info("Тестирование поля 'Имя' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         person = PersonData()
+
         with allure.step(f"Заполнение поля 'Имя' значением {person.first_name}"):
-            logger.info(f"Заполнение поля 'Имя' значением {person.first_name}")
             page.fill_first_name(person.first_name)
+
         with allure.step("Проверка корректности заполнения поля 'Имя'"):
-            logger.info("Проверка корректности заполнения поля 'Имя'")
             assert page.get_first_name() == person.first_name, \
                 f"Name {person.first_name} was not filled correctly"
+
         logger.info("Тестирование поля 'Имя' завершено")
 
     @allure.feature("Тестирование поля 'Фамилия'")
@@ -53,19 +54,21 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Фамилия'")
     @allure.description("Этот тест проверяет корректность заполнения "
                         "поля 'Фамилия' в форме регистрации.")
-    def test_last_name(self, page):
+    def test_last_name(self, page, caplog, logger):
         logger.info("Тестирование поля 'Фамилия' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         person = PersonData()
+
         with allure.step(f"Заполнение поля 'Фамилия' значением {person.last_name}"):
-            logger.info(f"Заполнение поля 'Фамилия' значением {person.last_name}")
             page.fill_last_name(person.last_name)
+
         with allure.step("Проверка корректности заполнения поля 'Фамилия'"):
-            logger.info("Проверка корректности заполнения поля 'Фамилия'")
             assert page.get_last_name() == person.last_name, \
                 f"Name {person.last_name} was not filled correctly"
+
         logger.info("Тестирование поля 'Фамилия' завершено")
 
     @allure.feature("Тестирование поля 'Email'")
@@ -73,19 +76,21 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Email'")
     @allure.description("Этот тест проверяет корректность "
                         "заполнения поля 'Email' в форме регистрации.")
-    def test_email(self, page):
+    def test_email(self, page, caplog, logger):
         logger.info("Тестирование поля 'Email' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         person = PersonData()
+
         with allure.step(f"Заполнение поля 'Email' значением {person.email}"):
-            logger.info(f"Заполнение поля 'Email' значением {person.email}")
             page.fill_email(person.email)
+
         with allure.step("Проверка корректности заполнения поля 'Email'"):
-            logger.info("Проверка корректности заполнения поля 'Email'")
             assert page.get_email() == person.email, \
                 f"Email {person.email} was not filled correctly"
+
         logger.info("Тестирование поля 'Email' завершено")
 
     @allure.feature("Тестирование выбора пола")
@@ -94,18 +99,19 @@ class TestRegistrationForm:
     @allure.description("Этот тест проверяет корректность "
                         "выбора пола в форме регистрации.")
     @pytest.mark.parametrize("gender", ["Male", "Female", "Other"])
-    def test_gender(self, page, gender):
+    def test_gender(self, page, gender, caplog, logger):
         logger.info("Тестирование выбора пола начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         with allure.step(f"Выбор пола: {gender}"):
-            logger.info(f"Выбор пола: {gender}")
             page.select_gender(gender)
+
         with allure.step(f"Проверка корректности выбора пола {gender}"):
-            logger.info(f"Проверка корректности выбора пола {gender}")
             assert page.is_gender_selected(gender), \
                 f"Gender {gender} was not selected correctly"
+
         logger.info("Тестирование выбора пола завершено")
 
     @allure.feature("Тестирование поля 'Мобильный телефон'")
@@ -113,21 +119,22 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Мобильный телефон'")
     @allure.description("Этот тест проверяет корректность заполнения поля "
                         "'Мобильный телефон' в форме регистрации.")
-    def test_mobile(self, page):
+    def test_mobile(self, page, caplog, logger):
         logger.info("Тестирование поля 'Мобильный телефон' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         person = PersonData()
+
         with allure.step(f"Заполнение поля 'Мобильный телефон' "
                          f"значением {person.mobile}"):
-            logger.info(f"Заполнение поля 'Мобильный телефон' "
-                        f"значением {person.mobile}")
             page.fill_mobile(person.mobile)
+
         with allure.step("Проверка корректности заполнения поля 'Мобильный телефон'"):
-            logger.info("Проверка корректности заполнения поля 'Мобильный телефон'")
             assert page.get_mobile() == person.mobile, \
                 f"Mobile {person.mobile} was not filled correctly"
+
         logger.info("Тестирование поля 'Мобильный телефон' завершено")
 
     @allure.feature("Тестирование выбора хобби")
@@ -144,18 +151,19 @@ class TestRegistrationForm:
         ["Reading", "Music"],
         ["Sports", "Reading", "Music"]
     ])
-    def test_hobbies(self, page, hobbies):
+    def test_hobbies(self, page, hobbies, caplog, logger):
         logger.info("Тестирование выбора хобби начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         with allure.step(f"Выбор хобби: {hobbies}"):
-            logger.info(f"Выбор хобби: {hobbies}")
             page.select_hobbies(hobbies)
+
         with allure.step("Проверка корректности выбора хобби"):
-            logger.info("Проверка корректности выбора хобби")
             assert page.are_hobbies_selected(hobbies), \
                 f"Hobbies {hobbies} were not selected correctly"
+
         logger.info("Тестирование выбора хобби завершено")
 
     @allure.feature("Тестирование загрузки изображения")
@@ -163,26 +171,28 @@ class TestRegistrationForm:
     @allure.title("Тест загрузки изображения")
     @allure.description("Этот тест проверяет корректность "
                         "загрузки изображения в форме регистрации.")
-    def test_upload_picture(self, page):
+    def test_upload_picture(self, page, caplog, logger):
         logger.info("Тестирование загрузки изображения начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         with allure.step("Scroll вниз страницы"):
-            logger.info("Scroll вниз страницы")
             page.scroll_to_down()
+
         project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = os.path.join(project_root, "images", "Battlestar_Galactica.jpg")
+
         with allure.step(f"Проверка существования файла {file_path}"):
-            logger.info(f"Проверка существования файла {file_path}")
             assert os.path.exists(file_path), f"File {file_path} does not exist"
+
         with allure.step(f"Загрузка изображения {file_path}"):
-            logger.info(f"Загрузка изображения {file_path}")
             page.upload_picture(file_path)
+
         with allure.step("Проверка корректности загрузки изображения"):
-            logger.info("Проверка корректности загрузки изображения")
             assert page.is_picture_uploaded(file_path), \
                 f"Picture {file_path} was not uploaded correctly"
+
         logger.info("Тестирование загрузки изображения завершено")
 
     @allure.feature("Тестирование поля 'Дата рождения'")
@@ -190,20 +200,21 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Дата рождения'")
     @allure.description("Этот тест проверяет корректность "
                         "заполнения поля 'Дата рождения' в форме регистрации.")
-    def test_date_of_birth(self, page):
+    def test_date_of_birth(self, page, caplog, logger):
         logger.info("Тестирование поля 'Дата рождения' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         date_of_birth = "08-March-1987"
         with allure.step(f"Заполнение поля 'Дата рождения' значением {date_of_birth}"):
-            logger.info(f"Заполнение поля 'Дата рождения' значением {date_of_birth}")
             page.fill_date_of_birth(date_of_birth)
+
         expected_date = "08 Mar 1987"
         with allure.step("Проверка корректности заполнения поля 'Дата рождения'"):
-            logger.info("Проверка корректности заполнения поля 'Дата рождения'")
             assert page.get_date_of_birth() == expected_date, \
                 f"Date of Birth {date_of_birth} was not filled correctly"
+
         logger.info("Тестирование поля 'Дата рождения' завершено")
 
     @allure.feature("Тестирование выбора предметов")
@@ -216,17 +227,18 @@ class TestRegistrationForm:
         ["Computer Science"],
         ["English", "Chemistry", "Biology", "Social Studies", "Physics"]
     ])
-    def test_subjects(self, page, subjects):
+    def test_subjects(self, page, subjects, caplog, logger):
         logger.info("Тестирование выбора предметов начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         with allure.step(f"Выбор предметов: {subjects}"):
-            logger.info(f"Выбор предметов: {subjects}")
             page.fill_subjects(subjects)
+
         with allure.step("Проверка корректности выбора предметов"):
-            logger.info("Проверка корректности выбора предметов")
             assert page.get_subjects() == subjects
+
         logger.info("Тестирование выбора предметов завершено")
 
     @allure.feature("Тестирование поля 'Текущий адрес'")
@@ -234,20 +246,22 @@ class TestRegistrationForm:
     @allure.title("Тест заполнения поля 'Текущий адрес'")
     @allure.description("Этот тест проверяет корректность"
                         " заполнения поля 'Текущий адрес' в форме регистрации.")
-    def test_current_address(self, page):
+    def test_current_address(self, page, caplog, logger):
         logger.info("Тестирование поля 'Текущий адрес' начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         person = PersonData()
+
         with allure.step(f"Заполнение поля 'Текущий адрес' "
                          f"значением {person.current_address}"):
-            logger.info("Заполнение поля 'Текущий адрес'")
             page.fill_current_address(person.current_address)
+
         with allure.step("Проверка корректности заполнения поля 'Текущий адрес'"):
-            logger.info("Проверка корректности заполнения поля 'Текущий адрес'")
             assert page.get_current_address() == person.current_address, \
                 f"Address {person.current_address} was not filled correctly"
+
         logger.info("Тестирование поля 'Текущий адрес' завершено")
 
     @allure.feature("Тестирование выбора штата и города")
@@ -261,26 +275,27 @@ class TestRegistrationForm:
         ("Haryana", "Karnal"),
         ("Rajasthan", "Jaipur")
     ])
-    def test_state_city(self, page, state, city, browser):
+    def test_state_city(self, page, state, city, browser, caplog, logger):
         logger.info(f"Тестирование выбора штата {state} и города {city} начато")
+
         with allure.step("Открытие страницы регистрации"):
-            logger.info("Открытие страницы регистрации")
             page.open()
+
         with allure.step("Scroll вниз страницы"):
-            logger.info("Scroll вниз страницы")
             page.scroll_to_bottom_page()
+
         with allure.step(f"Выбор штата: {state}"):
-            logger.info(f"Выбор штата: {state}")
             page.select_state(state)
+
         with allure.step(f"Выбор города: {city}"):
-            logger.info(f"Выбор города: {city}")
             page.select_city(city)
+
         with allure.step(f"Проверка корректности выбора штата {state}"):
-            logger.info(f"Проверка корректности выбора штата {state}")
             assert page.is_state_selected(state), \
                 f"State {state} was not selected correctly"
+
         with allure.step(f"Проверка корректности выбора города {city}"):
-            logger.info(f"Проверка корректности выбора города {city}")
             assert page.is_city_selected(city), \
                 f"City {city} was not selected correctly"
+
         logger.info(f"Тестирование выбора штата {state} и города {city} завершено")
